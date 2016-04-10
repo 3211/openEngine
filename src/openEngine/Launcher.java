4bbebcf8caf5c3adebaa7e7847de25c9ae619851
AAudette <nexus3211@gmail.com>
2016-04-10 14:44:48 -0700

    renamed some things

--- src/openEngine/Launcher.java
+++ src/openEngine/Launcher.java
@@ -3,7 +3,7 @@ package openEngine;
 import GUI.MainFrame;
 import GUI.MainThread;
 
-public class Driver {
+public class Launcher {
 	private static MainThread thread;
 	public static void main(String[] args){
 		thread = new MainThread(new CurrentProgram(new MainFrame()));	
